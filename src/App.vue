fc0aa3fde189cb68bc34d0fa27d95aaae1ecbd10
PaulDenis <78157298+PaulDenis@users.noreply.github.com>
2023-09-17 17:30:45 +0200

    Started to work on the nav_bar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <NavBar/>
+    <Homepage/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Homepage from './components/HomePage.vue'
+import NavBar from './components/NavBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+    Homepage,
+    NavBar
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import './Style/common.scss';
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  // font-family: 'Poiret One', 'Arial', 'Sans Serif';
 }
+// #app {
+//   font-family: Avenir, Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+//   margin-top: 60px;
+// }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="NavBar">
+        <img class="logo" src="../assets/logo-placeholder-image.png">
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+.NavBar {
+    height: 50px;
+    background-color: rgb(13, 37, 173);
+}
+.logo {
+    height: 100%;
+    width: auto;
+}
+</style>
