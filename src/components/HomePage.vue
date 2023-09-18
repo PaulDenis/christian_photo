fc01b234510915a8eb2dfc91a9f2ca98834fc29e
PaulDenis <78157298+PaulDenis@users.noreply.github.com>
2023-09-18 19:13:09 +0200

    Started working on the homepage Title and menu created.

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,6 +1,12 @@
 <template>
    <div class="banner">
       <h1>Photoentropy</h1>
+      <ul class="orizontal_menu">
+         <li :class="pagina_corrente == 'home' ? 'current' : ''">Home</li>
+         <li>My work</li>
+         <li>About me</li>
+         <li>Contacts</li>
+      </ul>
    </div>
 </template>
   
@@ -10,6 +16,11 @@
       props: {
 
       },
+      data() {
+         return {
+            pagina_corrente: "home"
+         }
+      }
    }
 </script>
   
@@ -18,11 +29,11 @@
   
    .banner {
       position: absolute;
-      bottom: 10%;
-      height: 10%;
+      bottom: 15%;
+      // height: 10%;
       width: 100%;
       text-align: center;
-      background-color: rgba(0, 0, 0, 0.4);
+      background-color: rgba(0, 0, 0, 0.6);
       color: white;
 
       // &:hover {
@@ -34,6 +45,19 @@
       h1 {
          margin-top: 10px;
       }
+   }
+   .orizontal_menu {
+      list-style: none;
+      margin-top: 20px;
+      margin-bottom: 20px;
+      li {
+         display: inline-block;
+         margin-right: 15px;
+      }
+   }
+
+   .current {
+      border-bottom: 2px solid blue;
    }
 </style>
   
