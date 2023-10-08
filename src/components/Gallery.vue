660f099acefe72a7705e37b456cc270fded48c25
PaulDenis <78157298+PaulDenis@users.noreply.github.com>
2023-10-08 11:54:06 +0200

    Gallery component created and liked to Home

---
+++ src/components/Gallery.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+      name: 'HomePage',
+      props: {
+         current_page: String
+      },
+      data() {
+         return {
+         }
+      }
+   }
+</script>
+
+<style>
+
+</style>
